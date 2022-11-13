01d78fcd86507ab7312cb9b8545fac0eef8277f6
achillies08 <vickypandey5250@gmail.com>
2022-11-13 20:46:51 +0530

    sfi

--- porobability_server/model.js
+++ porobability_server/model.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config()
 async function predict(ar) {
   const model = await tf.loadLayersModel(
-    "http://localhost:5000/model/model.json"
+    "https://porobabilityserver.herokuapp.com/model/model.json"
   );
   ar = tf.tensor(ar, [1, ar.length]);
   let prediction = await model.predict(ar).data();
